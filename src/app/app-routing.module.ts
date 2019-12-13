5bfcefb626b1b4e356e32d31249c62d2e5f6a7b0
lenniwnx <lwi@nexiosit.com>
2019-12-13 13:43:59 +0100

    working, probably!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {FormComponent} from './components/form/form.component';
 import {ConfirmationComponent} from './components/confirmation/confirmation.component';
+import {WallComponent} from './components/wall/wall.component';
 
 
 const routes: Routes = [
   {path: 'form', component: FormComponent},
-  {path: 'confirmation', component: ConfirmationComponent}
+  {path: 'confirmation', component: ConfirmationComponent},
+  {path: 'wall', component: WallComponent}
 ];
 
 @NgModule({
--- src/app/components/wall/wall.component.ts
+++ src/app/components/wall/wall.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ApiService} from '../../services/api.service';
 import {Character} from '../../../model/Character';
+import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-wall',
@@ -9,12 +10,34 @@ import {Character} from '../../../model/Character';
 })
 export class WallComponent implements OnInit {
 
-  characters: Character[];
+  characters: SanitizedCharacter[] = [];
 
-  constructor(private apiService: ApiService) { }
+  constructor(
+    private apiService: ApiService,
+    private sanitizer: DomSanitizer
+  ) { }
 
   ngOnInit() {
-    this.apiService.getAll().subscribe(characters => this.characters = characters);
+    this.apiService.getAll().subscribe(characters => {
+      characters.forEach( char => {
+        this.characters.push(new SanitizedCharacter(
+          char,
+          this.sanitizer.bypassSecurityTrustResourceUrl('data:image/jpg;base64,' + char.characterImg)
+
+        ));
+        this.characters.reverse();
+      });
+    });
   }
 
+}
+
+export class SanitizedCharacter {
+  char: Character;
+  sanitizedImage: SafeResourceUrl;
+
+  constructor(char: Character, sanitizedImage: SafeResourceUrl) {
+    this.char = char;
+    this.sanitizedImage = sanitizedImage;
+  }
 }
